6e808e468a782cfe61d94a8ec2788ae23c460a55
FlorianCliquet <florian.cliquet.pro@gmail.com>
2024-09-25 11:44:48 +0200

    added README.MD

--- ESIR1/SYS-S5/TPs/TP Unix/srcs/sys1_part2.sh
+++ ESIR1/SYS-S5/TPs/TP Unix/srcs/sys1_part2.sh
@@ -11,35 +11,39 @@ error_exit() {
     exit 1 # Exit status method 1 means failure
 }
 
+main() {
+	# Store the image in a variable for better readability
+	image_file="$1"
+
+	# Check if file exists
+	if [ ! -f "$image_file" ]; then
+	    error_exit "Error: File not found!"
+	fi
+
+	# Create output filenames
+	output_square="${image_file%.jpg}_square.jpg"
+	output_pdf="${image_file%.jpg}.pdf"
+
+	# Check if the output files already exist to avoid overwriting and weird interactions
+	[ -f "$output_square" ] && error_exit "Error: Intermediate file $output_square already exists."
+	[ -f "$output_pdf" ] && error_exit "Error: Output PDF $output_pdf already exists."
+
+	# Get the smallest dimension of the image
+	smallest_dim=$(identify -format "%[fx:min(w,h)]" "$image_file")
+
+	# Crop the image
+	convert -gravity center -crop "${smallest_dim}x${smallest_dim}+0+0!" "$image_file" "$output_square"
+
+	# Convert to PDF
+	img2pdf --output "$output_pdf" --pagesize 10cmx10cm "$output_square"
+
+	# Write the name of the PDF file to stdout
+	echo "Processing completed: PDF saved as $output_pdf"
+}
+
 # Check if exactly one argument is provided
 if [ "$#" -ne 1 ]; then
-    error_exit "Usage: $0 <image_file>"
+        error_exit "Usage: $0 <image_file>"
 fi
 
-# Store the image in a variable for better readability
-image_file="$1"
-
-# Check if file exists
-if [ ! -f "$image_file" ]; then
-    error_exit "Error: File not found!"
-fi
-
-# Create output filenames
-output_square="${image_file%.jpg}_square.jpg"
-output_pdf="${image_file%.jpg}.pdf"
-
-# Check if the output files already exist to avoid overwriting and weird interactions
-[ -f "$output_square" ] && error_exit "Error: Intermediate file $output_square already exists."
-[ -f "$output_pdf" ] && error_exit "Error: Output PDF $output_pdf already exists."
-
-# Get the smallest dimension of the image
-smallest_dim=$(identify -format "%[fx:min(w,h)]" "$image_file")
-
-# Crop the image
-convert -gravity center -crop "${smallest_dim}x${smallest_dim}+0+0!" "$image_file" "$output_square"
-
-# Convert to PDF
-img2pdf --output "$output_pdf" --pagesize 10cmx10cm "$output_square"
-
-# Write the name of the PDF file to stdout
-echo "Processing completed: PDF saved as $output_pdf"
+main "$1"
