b7c8bf165e6b37d9f9078ee12f8e1d6bd530a544
FlorianCliquet <florian.cliquet.pro@gmail.com>
2025-03-25 16:32:48 +0100

    corrected assert to tp2

--- ESIR1/PROG2/TPs/tp2_Liste/test/test_p3.cc
+++ ESIR1/PROG2/TPs/tp2_Liste/test/test_p3.cc
@@ -30,12 +30,13 @@ TEST(TestList, q311Find)
   Liste<std::string>::iterator it2(liste.end());
   Liste<std::string>::iterator it3(find(it1, it2, x));
   ASSERT_TRUE(it3 != it2) << "Les 2 itérateurs devraient être différents";
-  ASSERT_TRUE(it3 != it2) << "Les 2 itérateurs devraient être différents";
   ASSERT_TRUE(*it3 == x) << "it3 devrait pointer sur " << x;
+  
   x = "666666";
   it3 = find(it1, it2, x);
   ASSERT_TRUE(it3 != it2) << "Les 2 itérateurs devraient être différents";
   ASSERT_TRUE(*it3 == x) << "it3 devrait pointer sur " << x;
+  
   ++it1;
   ++it1;
   x = "FFFFFF";
@@ -49,4 +50,189 @@ TEST(TestList, q311FindListeVide)
   Liste<double> liste;
   Liste<double>::iterator pos = find(liste.begin(), liste.end(), 3.141519);
   ASSERT_TRUE(liste.end() == pos) << "Erreur recherche liste vide";
+}
+
+// test find dans liste d'un élément
+TEST(TestList, q311FindListe1Element)
+{
+  Liste<double> liste;
+  liste.push_back(3.141519);
+  
+  // Élément absent
+  Liste<double>::iterator pos = find(liste.begin(), liste.end(), -10.5);
+  ASSERT_TRUE(liste.end() == pos) << "Erreur recherche élément absent";
+
+  // Élément présent
+  pos = find(liste.begin(), liste.end(), 3.141519);
+  ASSERT_TRUE(liste.begin() == pos) << "Erreur recherche élément présent";
+}
+
+// test find dans liste de n éléments
+TEST(TestList, q311FindListe)
+{
+  Liste<double> liste;
+  std::deque<double> array;
+  ajouter_elts(liste, array);
+
+  for (unsigned int i = 0; i < array.size(); ++i) {
+    // Élément présent
+    Liste<double>::iterator pos = find(liste.begin(), liste.end(), array[i]);
+    ASSERT_TRUE(pos != liste.end()) << "Erreur élément présent indice " << i;
+    ASSERT_TRUE(*pos == array[i]) << "Valeur incorrecte à l'indice " << i;
+
+    // Élément absent
+    pos = find(liste.begin(), liste.end(), -array[i]-1);
+    ASSERT_TRUE(pos == liste.end()) << "Erreur élément absent devrait être introuvable";
+  }
+}
+
+//------------------------------------------------------------------------
+// tests insert q3.1.2
+//------------------------------------------------------------------------
+#ifndef VALGRIND
+TEST(TestList, q312InsertCurrentListAssertion)
+{
+  Liste<double> liste1;
+  liste1.push_back(3.14159);
+  Liste<double> liste2;
+  liste2.push_back(-77.77); 
+  ASSERT_DEATH(liste1.insert(liste2.begin(), 3.14159), "Assertion.*failed");
+}
+#endif
+
+TEST(TestList, q312InsertListeVide)
+{
+  Liste<double> liste;
+  Liste<double>::iterator new_elt = liste.insert(liste.begin(), 3.14159);
+  
+  ASSERT_TRUE(!liste.empty());
+  ASSERT_TRUE(liste.size() == 1);
+  ASSERT_TRUE(*liste.begin() == 3.14159);
+  ASSERT_TRUE(new_elt == liste.begin());
+}
+
+void inserer_elts(Liste<double>& liste, std::deque<double>& array, Position ici = alea)
+{
+  const int array_size = 100 + rand() % 100;
+  for (int i = 0; i < array_size; ++i) {
+    double x = 5 + drand48();
+    
+    if (ici == front) {
+      array.push_front(i * x * x);
+      Liste<double>::iterator new_elt = liste.insert(liste.begin(), i * x * x);
+      
+      ASSERT_TRUE(array.size() == liste.size());
+      ASSERT_TRUE(*array.begin() == *liste.begin());
+      ASSERT_TRUE(new_elt == liste.begin());
+    }
+    else if (ici == back) {
+      array.push_back(i * x + 1);
+      Liste<double>::iterator new_elt = liste.insert(liste.end(), i * x + 1);
+      
+      ASSERT_TRUE(array.size() == liste.size());
+      ASSERT_TRUE(*--array.end() == *--liste.end());
+      ASSERT_TRUE(new_elt == --liste.end());
+    }
+    else {
+      int pos = rand() % (1 + array.size());
+      auto liste_it = liste.begin();
+      auto array_it = array.begin();
+      
+      for (int p = 0; p < pos; ++p) { ++liste_it; ++array_it; }
+      
+      liste_it = liste.insert(liste_it, i * i * x + 1);
+      array_it = array.insert(array_it, i * i * x + 1);
+      
+      ASSERT_TRUE(*array_it == *liste_it);
+    }
+  }
+}
+
+TEST(TestList, q312InsertTeteListe)
+{
+  Liste<double> liste;
+  std::deque<double> array;
+  ajouter_elts(liste, array);
+  
+  inserer_elts(liste, array, front);
+  list_equals_deque_forward(liste, array);
+}
+
+TEST(TestList, q312InsertFinListe)
+{
+  Liste<double> liste;
+  std::deque<double> array;
+  ajouter_elts(liste, array);
+  
+  inserer_elts(liste, array, back);
+  list_equals_deque_forward(liste, array);
+}
+
+TEST(TestList, q312InsertHasardListe)
+{
+  Liste<double> liste;
+  std::deque<double> array;
+  ajouter_elts(liste, array);
+  
+  inserer_elts(liste, array, alea);
+  list_equals_deque_forward(liste, array);
+}
+
+//------------------------------------------------------------------------
+// tests erase q3.1.3
+//------------------------------------------------------------------------
+#ifndef VALGRIND
+TEST(TestList, q313EraseCurrentListAssertion)
+{
+  Liste<double> liste1;
+  liste1.push_back(3.14159);
+  Liste<double> liste2;
+  liste2.push_back(-77.77); 
+  ASSERT_DEATH(liste1.erase(liste2.begin()), "Assertion.*failed");
+}
+#endif
+
+#ifndef VALGRIND
+TEST(TestList, q313EraseEndListAssertion)
+{
+  Liste<double> liste;
+  ASSERT_DEATH(liste.erase(liste.begin()), "Assertion.*failed");
+}
+#endif
+
+TEST(TestList, q313EraseListe1Element)
+{
+  Liste<double> liste;
+  liste.push_back(3.14159);
+  Liste<double>::iterator next = liste.erase(liste.begin());
+  
+  ASSERT_TRUE(liste.empty());
+  ASSERT_TRUE(next == liste.end());
+}
+
+TEST(TestList, q313Erase)
+{
+  Liste<double> liste;
+  std::deque<double> array;
+  ajouter_elts(liste, array);
+  
+  for (unsigned int i = 0; i < array.size(); ++i) {
+    int pos = rand() % array.size();
+    auto liste_it = liste.begin();
+    auto array_it = array.begin();
+    
+    for (int p = 0; p < pos; ++p) { ++liste_it; ++array_it; }
+    
+    liste_it = liste.erase(liste_it);
+    array_it = array.erase(array_it);
+    
+    ASSERT_TRUE(array.size() == liste.size());
+    ASSERT_TRUE((array_it == array.end()) == (liste_it == liste.end()));
+    
+    if (array_it != array.end()) {
+      ASSERT_TRUE(*array_it == *liste_it);
+    }
+    
+    list_equals_deque_forward(liste, array);
+  }
 }
--- ESIR1/PROG2/TPs/tp2_Liste/test/test_p32.cc
+++ ESIR1/PROG2/TPs/tp2_Liste/test/test_p32.cc
@@ -26,58 +26,149 @@
 TEST(TestList, q321RechercheListeVide)
 {
   Liste<double> liste;
-  Liste<double>::iterator found = chercherTri(liste.begin(),
-					      liste.end(),
-					      3.14159);
-  ASSERT_TRUE(
-	    liste.end() == found)
-    << "Erreur itérateur résultat";
+  Liste<double>::iterator found = chercherTri(liste.begin(), liste.end(), 3.14159);
+  ASSERT_TRUE(liste.end() == found) << "Erreur itérateur résultat";
 }
 
 //------------------------------------------------------------------------
 // test recherche dans liste de taille 1
-// - élément inférieur au premier
-// - élément égal au premier
-// - élément supérieur au premier
 //------------------------------------------------------------------------
 TEST(TestList, q321RechercheListeTaille1)
 {
   Liste<double> liste;
   liste.push_back(3.14159);
-  // chercher un élément inférieur au premier
-  Liste<double>::iterator found = chercherTri(liste.begin(),
-					      liste.end(),
-					      -999.);
-  ASSERT_TRUE(
-	    liste.begin() == found)
-    << "Erreur itérateur résultat";
-  // chercher un élément égal au premier
-  found = chercherTri(liste.begin(),
-		      liste.end(),
-		      3.14159);
-  ASSERT_TRUE(
-	    liste.begin() == found)
-    << "Erreur itérateur résultat";
-  // chercher un élément supérieur au premier
-  found = chercherTri(liste.begin(),
-		      liste.end(),
-		      999.);
-  ASSERT_TRUE(
-	    liste.end() == found)
-    << "Erreur itérateur résultat";
+  
+  // Élément inférieur
+  Liste<double>::iterator found = chercherTri(liste.begin(), liste.end(), -999.);
+  ASSERT_TRUE(liste.begin() == found) << "Erreur itérateur résultat";
+
+  // Élément égal
+  found = chercherTri(liste.begin(), liste.end(), 3.14159);
+  ASSERT_TRUE(liste.begin() == found) << "Erreur itérateur résultat";
+
+  // Élément supérieur
+  found = chercherTri(liste.begin(), liste.end(), 999.);
+  ASSERT_TRUE(liste.end() == found) << "Erreur itérateur résultat";
 }
 
-// copier un tableau dans une liste
-void
-copier_array_list(
-		  const std::deque<double> & array,
-		  Liste<double> & liste
-		  )
+void copier_array_list(const std::deque<double>& array, Liste<double>& liste)
 {
-  std::deque<double>::const_iterator it     = array.begin();
-  std::deque<double>::const_iterator it_fin = array.end();
-  while (it != it_fin) {
+  for (auto it = array.begin(); it != array.end(); ++it) {
     liste.push_back(*it);
-    ++it;
   }
+}
+
+//------------------------------------------------------------------------
+// test recherche dans liste triée
+//------------------------------------------------------------------------
+TEST(TestList, q321RechercheListe)
+{
+  std::deque<double> array;
+  ajouter_elts(array);
+  std::sort(array.begin(), array.end());
+  
+  Liste<double> liste;
+  copier_array_list(array, liste);
+  
+  const double premier = array.front();
+  const double dernier = array.back();
+
+  // Test élément inférieur
+  auto found = chercherTri(liste.begin(), liste.end(), premier - 1000);
+  ASSERT_TRUE(liste.begin() == found) << "Erreur position élément inférieur";
+
+  // Test élément premier
+  found = chercherTri(liste.begin(), liste.end(), premier);
+  ASSERT_TRUE(liste.begin() == found) << "Erreur position premier élément";
+
+  // Test élément dernier
+  found = chercherTri(liste.begin(), liste.end(), dernier);
+  ASSERT_TRUE(--liste.end() == found) << "Erreur position dernier élément";
+
+  // Test élément supérieur
+  found = chercherTri(liste.begin(), liste.end(), dernier + 1000);
+  ASSERT_TRUE(liste.end() == found) << "Erreur position élément supérieur";
+
+  // Vérification de tous les éléments
+  for (const auto& val : array) {
+    found = chercherTri(liste.begin(), liste.end(), val);
+    ASSERT_TRUE(*found == val) << "Erreur valeur trouvée pour " << val;
+  }
+
+  // Tests aléatoires
+  for (int i = 0; i < 100; ++i) {
+    const double x = drand48() * 1000;
+    const auto array_it = std::lower_bound(array.begin(), array.end(), x);
+    found = chercherTri(liste.begin(), liste.end(), x);
+    
+    if (array_it == array.end()) {
+      ASSERT_TRUE(found == liste.end()) << "Erreur élément supérieur non détecté";
+    } else {
+      ASSERT_TRUE(*found == *array_it) << "Erreur valeur trouvée pour " << x;
+    }
+  }
+}
+
+//------------------------------------------------------------------------
+// test copie triée liste vide
+//------------------------------------------------------------------------
+TEST(TestList, q322CopieListeVide)
+{
+  Liste<double> original;
+  Liste<double>* copie = copierTri(original);
+  
+  ASSERT_TRUE(copie != nullptr) << "Pointeur null non autorisé";
+  ASSERT_TRUE(copie->empty()) << "Copie devrait être vide";
+  ASSERT_TRUE(copie->begin() == copie->end()) << "Itérateurs début/fin incorrects";
+  
+  delete copie;
+}
+
+//------------------------------------------------------------------------
+// test copie triée avec modifications
+//------------------------------------------------------------------------
+TEST(TestList, q322CopieListe)
+{
+  Liste<double> original;
+  std::deque<double> array;
+  ajouter_elts(original, array);
+  
+  Liste<double>* copie = copierTri(original);
+  std::sort(array.begin(), array.end());
+  
+  // Vérification copie initiale
+  list_equals_deque_forward(*copie, array);
+
+  // Modifications original
+  original.pop_front();
+  original.push_back(9999.99);
+  
+  // Vérification copie inchangée
+  list_equals_deque_forward(*copie, array);
+  
+  delete copie;
+}
+
+//------------------------------------------------------------------------
+// test copie triée avec modifications de la copie
+//------------------------------------------------------------------------
+TEST(TestList, q322CopieListeBis)
+{
+  Liste<double> original;
+  std::deque<double> array;
+  ajouter_elts(original, array);
+  
+  Liste<double>* copie = copierTri(original);
+  std::deque<double> array_trie = array;
+  std::sort(array_trie.begin(), array_trie.end());
+
+  // Modifications copie
+  copie->pop_front();
+  array_trie.pop_front();
+  
+  // Vérifications
+  list_equals_deque_forward(*copie, array_trie);
+  list_equals_deque_forward(original, array);
+  
+  delete copie;
 }
